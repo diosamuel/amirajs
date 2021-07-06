9b5342db76d60cf9a3a98b4c2238e195d50b9f34
Muhammad Fauzan <developerfauzan@gmail.com>
2021-07-06 15:07:56 +0700

    feat(): move to functional approach

--- amira.js
+++ amira.js
@@ -1,50 +1,44 @@
 /*! AmiraJS | (c) Muhammad Fauzan | github.com/fauzan121002/amirajs/license.md */ 
-class Amira {
-
-    // constructor for variable inside class object
-    constructor(t) {
-        (this.list = []), (this.built = document.querySelectorAll(t)), (this.nodeList = Array.prototype.forEach.call(this.built, (t) => this.list.push(t)));
-    }
-
-    // insert a value for input tag but also tags like div , span , textarea.
-    insertValue(t) {
-        this.list.forEach((i) => {
-            void 0 !== i.innerHTML && (i.innerHTML = t), (i.value = t);
-        });
-    }
-
-    // empty a value for input tag but also tags like div , span , textarea.
-    emptyValue() {
-        this.list.forEach((t) => {
-            void 0 !== t.innerHTML && (t.innerHTML = ""), (t.value = "");
-        });
-    }
-
-    // set attribute of a input tag but also tags like div , span , textarea.
-    setAttribute(t, i) {
-        this.list.forEach((s) => {
-            s.setAttribute(t, i);
-        });
-    }
-
-    // remove attribute of a input tag but also tags like div , span , textarea.
-    removeAttribute(t) {
-        this.list.forEach((i) => {
-            i.removeAttribute(t);
-        });
-    }
-
-    // show visibility of a input tag but also tags like div , span , textarea.
-    show() {
-        this.list.forEach((t) => {
-            t.style.visibility = "visible";
-        });
-    }
-
-    // hide visibility of a input tag but also tags like div , span , textarea.
-    hide() {
-        this.list.forEach((t) => {
-            t.style.visibility = "hidden";
-        });
+window.$ = function(selector){
+    return {
+        elements: (function() {
+            return Array.prototype.slice.call(document.querySelectorAll(selector));
+        })(),
+        insert: function(s) {
+            this.elements.forEach(t => {
+                void 0 !== t.innerHTML && (t.innerHTML = s), (t.value = s);
+            });
+            return this;
+        },
+        empty: function() {
+            this.elements.forEach(t => {
+                void 0 !== t.innerHTML && (t.innerHTML = ""), (t.value = "");
+            });
+            return this;
+        },
+        setAttr: function(a,s) {
+            this.elements.forEach(t => {
+                t.setAttr(a, s);
+            });
+            return this;
+        },
+        removeAttr: function(a) {
+            this.elements.forEach(t => {
+                t.removeAttr(a);
+            });
+            return this;
+        },
+        show: function() {
+            this.elements.forEach(t => {
+                t.style.visibility = "visible";
+            });
+            return this;
+        },
+        hide: function() {
+            this.elements.forEach(t => {
+                t.style.visibility = "hidden";
+            });
+            return this;
+        }
     }
 }
